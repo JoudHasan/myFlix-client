35185e6ecd0a48883ca98676473c2d78ed7dc895
Joud <joud.hasan85@gmail.com>
2024-01-17 20:16:38 +0100

    clean

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -64,30 +64,3 @@ export const LoginView = ({ onLoggedIn }) => {
     </Form>
   );
 };
-
-/*return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          maxLength="9"
-          required
-        />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
-*/
